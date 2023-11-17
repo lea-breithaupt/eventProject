a2facbfe8c1dbec75c939e9abbb2f28834a7ac84
lea-breithaupt <l.breithaupt3@gmail.com>
2023-11-17 11:29:55 -0700

    secondcommit

--- src/components/UserCreatedEvents.jsx
+++ src/components/UserCreatedEvents.jsx
@@ -3,28 +3,11 @@ import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const UserCreatedEvents = ({ eventList, setEventList }) => {
-  // const [userEvents, setUserEvents] = useState([])
-
-  // const [events, setEvents] = useState(eventDetails)
-  // const [updatedEvents, setUpdatedEvents] = useState(eventDetails)
-
-  // useEffect(() => {
-  //   fetchEvents();
-  // }, [setEventList]); // Update dependency to reload events after changes
-
-  // const fetchEvents = async () => {
-  //   try {
-  //     const response = await axios.post(`/addUserEvent/${userId}`)
-  //     setEventList(response.data);
-  //   } catch (error) {
-  //     console.error('Error fetching user events:', error);
-  //   }
-  // };
 
   const handleDeleteEvent = async (eventId) => {
       const response = await axios.delete(`/deleteEvent/${eventId}`)
       console.log(response.data)
-      setEventList(eventList.filter(event => event.eventId !== eventId));
+      setEventList(eventList.filter(event => event.eventId !== eventId))
     }
   
   return (
